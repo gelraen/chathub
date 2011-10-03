b6d8bb9cd0c5013afdc19e7f4edc4687ba8e8d74
Maxim Ignatenko <gelraen.ua@gmail.com>
2011-10-03 16:45:39 +0300

    Implement config file loading

--- src/chathub.erl
+++ src/chathub.erl
@@ -16,7 +16,32 @@ init(true) ->
     {ok, {{one_for_one, 10, 3}, Childs}}.
 
 load_config() ->
-    [].
+    File = get_config_path(),
+    case file:consult(File) of
+	{ok, Terms} ->
+	    Terms;
+	{error, {_, _, _} = Error} ->
+	    error_logger:error_msg("Unable to load config file ~s:~n~s",
+		[File, file:format_error(Error)]),
+	    exit(unable_to_parse_config);
+	{error, Error} ->
+	    error_logger:error_msg("Unable to open config file ~s: ~p",
+		[File, Error]),
+	    exit(unable_to_open_config)
+    end.
 
 start_hubs(Config) ->
     [].
+
+get_config_path() ->
+    case application:get_env(config) of
+	{ok, Path} ->
+	    Path;
+	undefined ->
+	    case os:getenv("CHATHUB_CONFIG") of
+		false ->
+		    "chathub.cfg";
+		Path ->
+		    Path
+	    end
+    end.
